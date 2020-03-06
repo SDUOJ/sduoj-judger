50999617876a086e33c99a254281412560646cb9
GeneLiuXe <356340460@qq.com>
2020-03-06 10:16:16 +0800

    modify details

--- judger.py
+++ judger.py
@@ -2,46 +2,17 @@ import subprocess
 import difflib
 from compiler import Compiler
 from fetcher import Fetcher
-
-from config import sandbox_path
-
-JUDGE_RESULT = [
-    "Accepted",
-    "CPU Time Limit Exceeded",
-    "Real Time Limit Exceeded",
-    "Memry Limit Exceeded",
-    "Runtime Error",
-    "System Error",
-    "Output Limit Exceeded",
-    "Wrong Answer",
-    "Presentation Error",
-]
+from config import sandbox_path, JUDGE_RESULT
 
 class Judger:
-    @staticmethod
-    '''
-    max_cpu_time, 
-    max_real_time, 
-    max_memory, 
-    max_stack, 
-    max_process_number, 
-    max_output_size, 
-    exe_path,
-    input_path, 
-    output_path, 
-    log_path, 
-    exe_args, 
-    exe_envs,
-    seccomp_rules,
-    uid,
-    gid
-    '''
-    def run(**kwargs):
+
+    @staticmethod    
+    def __run(**kwargs):
         int_args = ["max_cpu_time", "max_real_time", "max_memory", "max_stack", "max_process_number", "max_output_size", "uid", "gid"]
         str_args = ["exe_path", "input_path", "output_path", "log_path", "seccomp_rules"]
         str_list_args = ["exe_args", "exe_envs"]
 
-        command = sandbox_path
+        command = "sudo " + sandbox_path
 
         for arg in int_args:
             value = kwargs.get(arg, None)
@@ -56,28 +27,40 @@ class Judger:
         for arg in str_list_args:
             value = kwargs.get(arg, None)
             if isinstance(value, list):
-                command += " --{} {}".format(arg, str(item)) for item in value
-        
-        return subprocess.getstatusoutput(command)
+                command += " --{}={}".format(arg, str(value))
 
-        
+        return subprocess.getstatusoutput(command)     
 
-
-    def __compare(self, tid):
+    def __compare(self, tid, error = "WA"):
         path1 = "data/{}/output/output{}.txt".format(self._compile_config["pid"], tid)
         path2 = "test/{}/output/output{}.txt".format(self._compile_config["pid"], tid)
         command = "diff {} {}".format(path1, path2)
 
+        if error == "PE":
+            command += " --ignore-space-change --ignore-blank-lines"
+
         return subprocess.getstatusoutput(command)[0]
+
+    def __handle_error(self, error):
+        return
 
     # Return the result of one test
     def __one_judge(self, tid):
         # Use sandbox to run
-        judge_status, self.judge_result = Judger.run()
-        self.judge_result = json.loads(self.judge_result)
-        if self.__compare(tid): # wrong
-        # self.judge_result["result"] = "System Error" if judge_result or self.judge_result["error"] else JUDGE_RESULT[int(self.judge_result["result"])]
-        self.__compare(tid)
+        judge_status, self.judge_result = Judger.__run(
+            exe_path = "test/{}/main.cc".format(self._compile_config["pid"]),
+            input_path = "data/{}/input/input{}.txt".format(self._compile_config["pid"], tid),
+            output_path = "test/{}/output/output{}.txt".format(self._compile_config["pid"], tid),
+            seccomp_rules = "c_cpp"
+        )
+        if judge_status != 0:
+            self.judge_result["result"] = "System Error"
+        if self.judge_result["result"] == 0:
+            if self.__compare(tid):
+                self.judge_result["result"] = "Wrong Answer"    
+            if self.__compare(tid, "PE"):
+                self.judge_result["result"] = "Presentation Error"
+        return self.judge_result
 
 
     def __spj(self):
@@ -97,7 +80,7 @@ class Judger:
             return "CE"
 
         for i in range(1, 3):
-            self.__one_judge(i)
+            print(type(self.__one_judge(i)))
             # Handle the types of error
             if self.__compare(i):
                 print("WA on test", i)
