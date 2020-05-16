70782e4dd8551ce208531f13155a8166d04dc1ee
jeshrz <jeshrz063@gmail.com>
2020-05-16 22:57:25 +0800

    update: checkpointId and submissionId become INT bugfix: max memory limit

--- judger/model/checker.py
+++ judger/model/checker.py
@@ -3,9 +3,16 @@ import subprocess
 from judger.model.client import Judger
 
 
-def checker(test_output, std_output):
+def __checker(test_output, std_output):
     command = "diff {} {}".format(std_output, test_output)
     if subprocess.getstatusoutput(command)[0]:
         command += " --ignore-space-change --ignore-blank-lines"
         return Judger.WRONG_ANSWER if subprocess.getstatusoutput(command)[0] else Judger.PRESENTATION_ERROR
     return Judger.SUCCESS
+
+"""
+不检测pe的
+"""
+def checker(test_output, std_output):
+    command = "diff -q -Z {} {}".format(std_output, test_output)
+    return Judger.WRONG_ANSWER if subprocess.getstatusoutput(command)[0] else Judger.SUCCESS
