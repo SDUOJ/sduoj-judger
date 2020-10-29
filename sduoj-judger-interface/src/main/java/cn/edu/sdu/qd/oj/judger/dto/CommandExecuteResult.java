ff97314f0480b2513c6abb6322983f8bb257a3a8
zhangt2333 <zhangt2333@gmail.com>
2020-10-29 13:37:54 +0800

    command executor

---
+++ sduoj-judger-interface/src/main/java/cn/edu/sdu/qd/oj/judger/dto/CommandExecuteResult.java
@@ -0,0 +1,12 @@
+package cn.edu.sdu.qd.oj.judger.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommandExecuteResult extends BaseDTO {
+
+}
