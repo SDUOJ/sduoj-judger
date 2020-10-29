f070df39dbf5a563978b8be393398c5c938cf0cf
zhangt2333 <zhangt2333@gmail.com>
2020-10-29 19:04:07 +0800

    rabbit sender (#12)
    * rabbit sender
    
    * sleep for retry

---
+++ sduoj-judger-service/src/main/java/cn/edu/sdu/qd/oj/judger/sender/RabbitSender.java
@@ -0,0 +1,45 @@
+package cn.edu.sdu.qd.oj.judger.sender;
+
+import cn.edu.sdu.qd.oj.judger.dto.OneJudgeResult;
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+* @Description 封装一层工具层，用于发送 mq 消息
+**/
+@Slf4j
+@Component
+public class RabbitSender {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    /**
+    * @Description 发送单点评测结果
+    **/
+    public void sendOneJudgeResult(OneJudgeResult oneJudgeResult) {
+        for (int i = 0; i < 5; i++) {
+            try {
+                this.rabbitTemplate.convertAndSend(
+                        "sduoj.submission.exchange",
+                        "sduoj.submission.queue",
+                        JSON.toJSONString(oneJudgeResult)
+                );
+                break;
+            } catch (AmqpException e) {
+                log.warn("sendOneJudgeResult", e);
+                try {
+                    Thread.sleep(i * 2000);
+                } catch (Throwable ignore) {
+                }
+            } catch (Exception e) {
+                log.error("sendOneJudgeResult", e);
+                break;
+            }
+        }
+    }
+}
