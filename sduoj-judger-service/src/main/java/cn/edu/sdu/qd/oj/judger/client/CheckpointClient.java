80f493420099b1afbbc7af2fb85c123fe7db2e09
zhangt2333 <zhangt2333@gmail.com>
2020-11-24 22:46:45 +0800

    update license

--- sduoj-judger-service/src/main/java/cn/edu/sdu/qd/oj/judger/client/CheckpointClient.java
+++ sduoj-judger-service/src/main/java/cn/edu/sdu/qd/oj/judger/client/CheckpointClient.java
@@ -1,7 +1,16 @@
+/*
+ * Copyright 2020-2020 the original author or authors.
+ *
+ * Licensed under the General Public License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.gnu.org/licenses/gpl-3.0.en.html
+ */
+
 package cn.edu.sdu.qd.oj.judger.client;
 
 import cn.edu.sdu.qd.oj.checkpoint.api.CheckpointApi;
-import cn.edu.sdu.qd.oj.problem.api.ProblemApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @FeignClient(value = CheckpointApi.SERVICE_NAME, qualifier = "CheckpointClient")
