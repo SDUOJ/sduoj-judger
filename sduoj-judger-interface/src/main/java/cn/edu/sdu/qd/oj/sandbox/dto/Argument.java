257ae51984e403820b6dc8b043ae012069dbf7fb
Teng Zhang <zhangt2333@gmail.com>
2021-04-22 00:31:55 +0800

    bugfix

--- sduoj-judger-interface/src/main/java/cn/edu/sdu/qd/oj/sandbox/dto/Argument.java
+++ sduoj-judger-interface/src/main/java/cn/edu/sdu/qd/oj/sandbox/dto/Argument.java
@@ -14,6 +14,7 @@ import cn.edu.sdu.qd.oj.judger.exception.SystemErrorException;
 import cn.edu.sdu.qd.oj.sandbox.enums.SandboxArgument;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -25,12 +26,16 @@ import java.util.Map;
  */
 public class Argument {
 
-    private Map<SandboxArgument, Object> args;
+    private final Map<SandboxArgument, Object> args;
 
     private Argument() {
+        this.args = new HashMap<>();
     }
 
     public Argument add(SandboxArgument key, Object value) throws SystemErrorException {
+        if (key == null || value == null) {
+            return this;
+        }
         if (!key.clz.isInstance(value)) {
             throw new SystemErrorException(String.format("Unexpected sandbox argument type of %s: %s\n", key, value.getClass()));
         }
@@ -41,16 +46,14 @@ public class Argument {
     public List<String> getFormatArgs() {
         List<String> list = new ArrayList<>();
         args.forEach((k, v) -> {
-            if (k != null && v != null) {
-                if (String.class == k.clz) {
-                    list.add(String.format("--%s=\"%s\"", k, v));
-                } else if (String[].class == k.clz) {
-                    for (String _v : (String[]) v) {
-                        list.add(String.format("--%s=\"%s\"", k, _v));
-                    }
-                } else {
-                    list.add(String.format("--%s=%s", k, v));
+            if (String.class == k.clz) {
+                list.add(String.format("--%s=\"%s\"", k, v));
+            } else if (String[].class == k.clz) {
+                for (String _v : (String[]) v) {
+                    list.add(String.format("--%s=\"%s\"", k, _v));
                 }
+            } else {
+                list.add(String.format("--%s=%s", k, v));
             }
         });
         return list;
