167cde12d79e1453b891c5a573fce04382baaee9
Teng Zhang <zhangt2333@gmail.com>
2023-04-18 15:03:15 +0800

    feat: adapt to container name for docker stack

--- sduoj-judger-service/src/main/java/cn/edu/sdu/qd/oj/judger/config/DockerContainers.java
+++ sduoj-judger-service/src/main/java/cn/edu/sdu/qd/oj/judger/config/DockerContainers.java
@@ -10,11 +10,13 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * some utils for getting the infomation of the container which the judger is running in
+ * some utils for getting the information of the container which the judger is running in
  *
  * @author zhangt2333
  */
 public class DockerContainers {
+
+    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(DockerContainers.class);
 
     private static String CONTAINER_ID = null;
 
@@ -57,15 +59,22 @@ public class DockerContainers {
             Set<String> containerNames = new HashSet<>();
             for (String containerIp : getContainerIps()) {
                 ProcessUtils.CommandResult result = ShellUtils.execCmd("host", '"' + containerIp + '"');
-                if (result.exitCode != 0) {
-                    throw new RuntimeException("Get host failed, command output: "
-                            + result.stdout + "\n" + result.stderr);
+                if (result.exitCode == 0) {
+                    // result.stdout, e.g., "79.19.0.10.in-addr.arpa domain name pointer sduoj_judger.1.n5l9a4x5kmqgr81lzpxfqux0n.sduoj_network."
+                    // result.stdout, e.g., "7.3.190.172.in-addr.arpa domain name pointer sduoj_judger_1.sduoj_network."
+                    String stdout = result.stdout;
+                    // temp1, e.g., "sduoj_judger.1.n5l9a4x5kmqgr81lzpxfqux0n.sduoj_network."
+                    // temp1, e.g., "sduoj_judger_1.sduoj_network."
+                    String temp1 = stdout.substring(stdout.lastIndexOf(' ') + 1);
+                    // temp2, e.g., "sduoj_judger.1.n5l9a4x5kmqgr81lzpxfqux0n"
+                    // temp2, e.g., "sduoj_judger_1"
+                    String temp2 = temp1.substring(0, temp1.lastIndexOf('.', temp1.lastIndexOf('.') - 1));
+                    containerNames.add(temp2);
+                    logger.info("Get container name: {}", temp2);
+                } else {
+                    logger.warn("Get container name failed, command output: {}\n{}",
+                            result.stdout, result.stderr);
                 }
-                // temp1, e.g., "container-name-1.network1."
-                String temp1 = result.stdout.substring(result.stdout.lastIndexOf(' ') + 1);
-                // temp2, e.g., "container-name-1"
-                String temp2 = temp1.substring(0, temp1.indexOf('.'));
-                containerNames.add(temp2);
             }
             if (containerNames.size() != 1) {
                 throw new RuntimeException("Found zero or multiple container names, so confusing: "
@@ -82,17 +91,34 @@ public class DockerContainers {
     public static int getContainerNodeIndex() throws SystemErrorException {
         if (NODE_INDEX == -1) {
             String containerName = getContainerName();
+            String temp;
+            // "sduoj_judger.1.n5l9a4x5kmqgr81lzpxfqux0n" -> "1"
+            int i1 = containerName.indexOf('.');
+            int i2 = containerName.lastIndexOf('.');
+            if (i1 != -1 && i2 != -1 && i1 != i2) {
+                temp = containerName.substring(i1 + 1, i2);
+                if (StringUtils.isNumeric(temp)) {
+                    NODE_INDEX = Integer.parseInt(temp);
+                    return NODE_INDEX;
+                }
+            }
             // "sduoj_judger_1" -> "1"
-            String temp = containerName.substring(containerName.lastIndexOf('_') + 1);
-            if (StringUtils.isNumeric(temp)) {
-                NODE_INDEX = Integer.parseInt(temp);
-                return NODE_INDEX;
+            int i = containerName.lastIndexOf('_');
+            if (i != -1) {
+                temp = containerName.substring(i + 1);
+                if (StringUtils.isNumeric(temp)) {
+                    NODE_INDEX = Integer.parseInt(temp);
+                    return NODE_INDEX;
+                }
             }
             // "sduoj_judger-1" -> "1"
-            temp = containerName.substring(containerName.lastIndexOf('-') + 1);
-            if (StringUtils.isNumeric(temp)) {
-                NODE_INDEX = Integer.parseInt(temp);
-                return NODE_INDEX;
+            i = containerName.lastIndexOf('-');
+            if (i != -1) {
+                temp = containerName.substring(i + 1);
+                if (StringUtils.isNumeric(temp)) {
+                    NODE_INDEX = Integer.parseInt(temp);
+                    return NODE_INDEX;
+                }
             }
             if (NODE_INDEX == -1) {
                 throw new RuntimeException("Get container node index failed, container name: "
