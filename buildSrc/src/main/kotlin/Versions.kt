7002d04c88022488f1354f4b3f229375b457593d
Teng Zhang <zhangt2333@gmail.com>
2024-07-16 22:54:27 +0800

    feat: upgrade the underlying framework

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,13 +3,10 @@ object Versions {
 
     const val java = "17"
 
-    const val springBoot = "3.0.5"
-    const val springCloud = "2022.0.2"
-    const val springCloudAlibaba = "2022.0.0.0-RC1"
+    const val springBoot = "3.2.4"
+    const val springCloud = "2023.0.1"
+    const val springCloudAlibaba = "2023.0.1.0"
 
-    const val nacos = "2.2.1"
-
-    const val lombok = "1.18.26"
-    const val commonsLang3 = "3.11"
-    const val commonsIO = "2.11.0"
+    const val lombok = "1.18.34"
+    const val commonsLang3 = "3.14.0"
 }
